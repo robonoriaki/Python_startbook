7dd06b013dc7bac4b385984cfc016cb88c612eb4
robonoriaki <s1526038GQ@s.chibakoudai.jp>
2020-01-06 20:33:06 +0900

    Updata cleaner2 module

--- chapter10/cleaner2.py
+++ chapter10/cleaner2.py
@@ -8,29 +8,6 @@ import math
 
 PI = math.pi
 
-#継承するため、turtle.Turtleを指定する
-class Kame(turtle.Turtle):
-    #初期化メソッド
-    def __init__(self):
-        #親クラスであるturtle.Turtle型の初期化メソッドを呼び出す
-        super(Kame, self).__init__()
-        #その後、処理を加える
-        self.shape('turtle')
-        self.shapesize(2,2,3)
-        #ウィンドウの幅と高さを得る
-        #そこからx軸、y軸の最大値を得る
-        xe = self.getscreen().window_width() / 2.0
-        ye = self.getscreen().window_height() / 2.0
-        
-    #壁に当たってターンする動きをする 
-    def hit_wall(self):
-        #カメの角度、現在のx、y座標を渡す
-        line = Line(self.heading(), self.xcor, self.ycor)
-        rand_angle = random.random() * PI
-        
-        
-        
-
 
 #ウィンドウの幅と高さはすでにあるメソッドを使ってわかる、ウィンドウの中心が座標原点
 #つまり、高さhなら、プラス方向の限界はh/2である
@@ -57,3 +34,75 @@ class Line:
     def get_x(self, y):
         endx = self.x0 + (y - self.y0) / self.a
         return endx
+
+#継承するため、turtle.Turtleを指定する
+class Kame(turtle.Turtle):
+    #初期化メソッド
+    def __init__(self):
+        #親クラスであるturtle.Turtle型の初期化メソッドを呼び出す
+        super(Kame, self).__init__()
+        #その後、処理を加える
+        self.shape('turtle')
+        self.shapesize(2,2,3)
+        #ウィンドウの幅と高さを得る
+        #そこからx軸、y軸の最大値を得る
+        self.xe = self.getscreen().window_width() / 2.0
+        self.ye = self.getscreen().window_height() / 2.0
+        #ペンの幅を10にする
+        self.width(10)
+        #背景色を変更
+        self.getscreen().bgcolor('gray')
+        #ペンの色を変更
+        self.pencolor('white')
+        #度数法から弧度法に変更する
+        self.radians()
+                                
+        
+    #壁に当たってターンする動きをする 
+    def hit_wall(self):
+        #カメの角度、現在のx、y座標を渡す
+        line = Line(self.heading(), self.xcor(), self.ycor())
+        #角度を乱数で得る
+        rand_angle = random.random() * PI
+        #四隅までの角度を計算
+        upleft = self.towards(-1 * self.xe, self.ye)
+        upright = self.towards(self.xe, self.ye)
+        loleft = self.towards(-1 * self.xe, -1 * self.ye)
+        loright = self.towards(self.xe, -1 * self.ye)
+        
+        #上の辺にぶつかる場合
+        if upleft > self.heading() >= upright:
+            #print('up')
+            des_x = line.get_x(self.ye)
+            des_y = self.ye
+            #必ず内側を向くように調整
+            turn_angle = self.heading() + rand_angle
+        #左の辺にぶつかる場合
+        elif loleft > self.heading() >= upleft:
+            #print('left')
+            des_x = self.xe
+            des_y = line.get_y(self.xe)
+            turn_angle = self.heading() - 0.5 * PI + rand_angle
+        #下の辺にぶつかる場合
+        elif loright > self.heading() >= loleft:
+            #print('low')
+            des_x = line.get_x(self.ye)
+            des_y = -1 * self.ye
+            turn_angle = self.heading() - rand_angle
+        #右の辺にぶつかる場合
+        else:
+            #print('right')
+            des_x = self.xe
+            des_y = line.get_y(self.xe)
+            turn_angle = self.heading() - 0.5 * PI - rand_angle
+        
+        #壁にぶつかるまで移動
+        self.goto(des_x * 0.95, des_y * 0.95)
+        #回転
+        self.right(turn_angle)
+        #print('turn angle: {}'.format(turn_angle))
+        
+    #動き続ける
+    def run(self):
+        while True:
+            self.hit_wall()
